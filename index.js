e7559321a4facd7899234ec3e6e0856da009db34
Johnathan Hoyt <johnathanhoyt@Gabriellas-MacBook-Pro.local>
2020-06-11 21:31:38 -0400

    finished task 1, working on task 2

--- index.js
+++ index.js
@@ -41,8 +41,24 @@ class Airplane {
 */
 
 class Person {
-
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(someFood) {
+    if(this.stomach.length < 10) {
+      this.stomach.push(someFood);
+    }
+  }
+  poop() {
+    this.stomach = [];
+  }
+  toString() {
+    return `${this.name}, ${this.age}`;
+  }
 }
+const john = new Person('John', 24);
 
 /*
   TASK 2
@@ -59,9 +75,21 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    return (this.tank += gallons);
+  }
+  drive(distance) {
+    this.odometer += distance;
+    this.tank -= distance / this.milesPerGallon;
+  }
 }
-
+const car = new Car(mirage, 36);
 /*
   TASK 3
     - Write a Lambdasian class.
